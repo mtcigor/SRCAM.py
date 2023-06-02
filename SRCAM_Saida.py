071607de7fc8fa671c0a841fd2a042b60d636b56
Igor Costa <99365439+mtcigor@users.noreply.github.com>
2023-06-02 09:27:34 +0100

    linguagem para português

--- SRCAM_Saida.py
+++ SRCAM_Saida.py
@@ -16,21 +16,21 @@ import datetime
 import mysql.connector
 
 #Caminhos dos Ficheiros
-CUSTOM_MODEL_NAME = 'my_ssd_mobnet'
-LABEL_MAP_NAME = 'label_map.pbtxt'
+NOME_MODELO = 'my_ssd_mobnet'
+MAPA_LABEL_NOME = 'label_map.pbtxt'
 
-paths = {
-    'CHECKPOINT_PATH': os.path.join('Tensorflow', 'workspace','models', CUSTOM_MODEL_NAME),
-    'ANNOTATION_PATH': os.path.join('Tensorflow', 'workspace','annotations')
+caminho = {
+    'CHECKPOINT_CAMINHO': os.path.join('Tensorflow', 'workspace','models', NOME_MODELO),
+    'ANOTACAO_CAMINHO': os.path.join('Tensorflow', 'workspace','annotations')
 }
-files = {
-    'PIPELINE_CONFIG':os.path.join('Tensorflow', 'workspace','models', CUSTOM_MODEL_NAME, 'pipeline.config'),
-    'LABELMAP': os.path.join(paths['ANNOTATION_PATH'], LABEL_MAP_NAME) 
+ficheiro = {
+    'PIPELINE_CONFIG':os.path.join('Tensorflow', 'workspace','models', NOME_MODELO, 'pipeline.config'),
+    'MAPALABEL': os.path.join(caminho['ANOTACAO_CAMINHO'], MAPA_LABEL_NOME) 
 }
 labels = [{'name':'license', 'id':1}]
 
 #Mapa de classes
-with open(files['LABELMAP'], 'w') as f:
+with open(ficheiro['MAPALABEL'], 'w') as f:
     for label in labels:
         f.write('item { \n')
         f.write('\tname:\'{}\'\n'.format(label['name']))
@@ -38,38 +38,38 @@ with open(files['LABELMAP'], 'w') as f:
         f.write('}\n')
 
 # Carregar pieline e construir um modelo de detecção
-configs = config_util.get_configs_from_pipeline_file(files['PIPELINE_CONFIG'])
-detection_model = model_builder.build(model_config=configs['model'], is_training=False)
+configs = config_util.get_configs_from_pipeline_file(ficheiro['PIPELINE_CONFIG'])
+modelo_detecao = model_builder.build(model_config=configs['model'], is_training=False)
 
 # Restaurar checkpoint
-ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
-ckpt.restore(os.path.join(paths['CHECKPOINT_PATH'], 'ckpt-11')).expect_partial()
+ckpt = tf.compat.v2.train.Checkpoint(model=modelo_detecao)
+ckpt.restore(os.path.join(caminho['CHECKPOINT_CAMINHO'], 'ckpt-11')).expect_partial()
 
 # Função deteção
 @tf.function
-def detect_fn(image):
-    image, shapes = detection_model.preprocess(image)
-    prediction_dict = detection_model.predict(image, shapes)
-    detections = detection_model.postprocess(prediction_dict, shapes)
-    return detections
+def detect_fn(imagem):
+    imagem, formas = modelo_detecao.preprocess(imagem)
+    predicacao = modelo_detecao.predict(imagem, formas)
+    detecoes = modelo_detecao.postprocess(predicacao, formas)
+    return detecoes
 
 #Carregar as Classes
-category_index = label_map_util.create_category_index_from_labelmap(files['LABELMAP'])
+categoria = label_map_util.create_category_index_from_labelmap(ficheiro['MAPALABEL'])
 
 # Função filtragem
-def filter_text(region, ocr_result, region_threshold):
-    rectangle_size = region.shape[0]*region.shape[1]
+def filtro_texto(regiao, ocr_resultado, regiao_limite):
+    tamanho_retangulo = regiao.shape[0]*regiao.shape[1]
     
-    plate = [] 
-    for result in ocr_result:
-        length = np.sum(np.subtract(result[0][1], result[0][0]))
-        height = np.sum(np.subtract(result[0][2], result[0][1]))
+    caixa = [] 
+    for resultado in ocr_resultado:
+        comprimento = np.sum(np.subtract(resultado[0][1], resultado[0][0]))
+        altura = np.sum(np.subtract(resultado[0][2], resultado[0][1]))
         
-        if length*height / rectangle_size > region_threshold:
-            plate.append(result[1])
-    return plate
+        if comprimento*altura / tamanho_retangulo > regiao_limite:
+            caixa.append(resultado[1])
+    return caixa
 
-def levenshtein_distance(s1, s2):
+def levenshtein_distancia(s1, s2):
     m = len(s1)
     n = len(s2)
 
@@ -91,146 +91,166 @@ def levenshtein_distance(s1, s2):
 
     return dp[m][n]
 
-def find_closest_value(input_str):
-    connection = mysql.connector.connect(
+# Encontrar na tabela a matrícula mais semelhante ou igual(limite 4)
+def find_closest_matricula(input_str):
+    conexao = mysql.connector.connect(
         host="10.1.31.46",
         user="root",
         password="aedas",
         database="parkit"
     )
 
-    cursor = connection.cursor()
+    cursor = conexao.cursor()
 
-    query = "SELECT matricula FROM matriculas"
-    cursor.execute(query)
+    sql = "SELECT matricula FROM matriculas"
+    cursor.execute(sql)
 
-    closest_value = None
-    min_distance = float('inf')
+    valor_perto = None
+    distancia_min = float('inf')
 
     for row in cursor.fetchall():
-        value = row[0]
-        distance = levenshtein_distance(input_str, value)
-        if distance <= 3 and distance < min_distance:
-            closest_value = value
-            min_distance = distance
+        valor = row[0]
+        distancia = levenshtein_distancia(input_str, valor)
+        if distancia <= 4 and distancia < distancia_min:
+            valor_perto = valor
+            distancia_min = distancia
 
     cursor.close()
-    connection.close()
+    conexao.close()
 
-    if min_distance <= 3:
-        return closest_value
+    if distancia_min <= 4:
+        return valor_perto
     else:
         return 0
 
-
-def ocr_it(image, detections, detection_threshold, region_threshold):
+def ocr_it(imagem, detecoes, detecao_limite, regiao_limite):
     
-    # Scores, boxes and classes above threhold
-    scores = list(filter(lambda x: x> detection_threshold, detections['detection_scores']))
-    boxes = detections['detection_boxes'][:len(scores)]
-    classes = detections['detection_classes'][:len(scores)]
+    # Pontuações, caixas e classes acima do limite
+    pontos = list(filter(lambda x: x> detecao_limite, detecoes['detection_scores']))
+    caixas = detecoes['detection_boxes'][:len(pontos)]
+    classes = detecoes['detection_classes'][:len(pontos)]
     
-    # Full image dimensions
-    width = image.shape[1]
-    height = image.shape[0]
+    # Dimensões completas da imagem
+    largura = imagem.shape[1]
+    altura = imagem.shape[0]
     
-    # Apply ROI filtering and OCR
-    for idx, box in enumerate(boxes):
-        roi = box*[height, width, height, width]
-        region = image[int(roi[0]):int(roi[2]),int(roi[1]):int(roi[3])]
-        reader = easyocr.Reader(['en'])
-        ocr_result = reader.readtext(region)
+    # Aplicar filtragem ROI(Regiao de Interese) e OCR(Reconhecimento ótico de caracteres)
+    for idx, caixa in enumerate(caixas):
+        roi = caixa*[altura, largura, altura, largura]
+        regiao = imagem[int(roi[0]):int(roi[2]),int(roi[1]):int(roi[3])]
+        leitor = easyocr.Reader(['en'])
+        ocr_resultado = leitor.readtext(regiao)
         
-        text = filter_text(region, ocr_result, region_threshold)
+        texto = filtro_texto(regiao, ocr_resultado, regiao_limite)
         
-        plt.imshow(cv2.cvtColor(region, cv2.COLOR_BGR2RGB))
+        plt.imshow(cv2.cvtColor(regiao, cv2.COLOR_BGR2RGB))
         plt.show()
-        print(text)
-        return text, region
+        print(texto)
+        return texto, regiao
 
-# Salvar Resultados em CSV e Pasta com Imagens
-def save_results(text, region, csv_filename, folder_path):
-    img_name = '{}.jpg'.format(uuid.uuid1())
-    
-    cv2.imwrite(os.path.join(folder_path, img_name), region)
-    
-    with open(csv_filename, mode='a', newline='') as f:
-        csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow([img_name, text])
 
-detection_threshold = 0.1
-region_threshold = 0.1
+detecao_limite = 0.1
+regiao_limite = 0.1
 
 conn = mysql.connector.connect(host = '10.1.31.46', user = 'root', password = 'aedas', database = 'parkit')
 cursor = conn.cursor ()
 
 #Captura em Direto
 cap = cv2.VideoCapture(1)
-width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-times = 0
+largura = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+altura = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+tempos = 0
 
 while cap.isOpened(): 
     ret, frame = cap.read()
-    image_np = np.array(frame)
+    imagem_np = np.array(frame)
     
-    input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)
-    detections = detect_fn(input_tensor)
+    input_tensor = tf.convert_to_tensor(np.expand_dims(imagem_np, 0), dtype=tf.float32)
+    detecoes = detect_fn(input_tensor)
     
-    num_detections = int(detections.pop('num_detections'))
-    detections = {key: value[0, :num_detections].numpy()
-                  for key, value in detections.items()}
-    detections['num_detections'] = num_detections
+    num_detecoes = int(detecoes.pop('num_detections'))
+    detecoes = {key: value[0, :num_detecoes].numpy()
+                  for key, value in detecoes.items()}
+    detecoes['num_detections'] = num_detecoes
 
     # detection_classes should be ints.
-    detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+    detecoes['detection_classes'] = detecoes['detection_classes'].astype(np.int64)
 
-    label_id_offset = 1
-    image_np_with_detections = image_np.copy()
+    label_id_desvio = 1
+    imagem_np_com_detecoes = imagem_np.copy()
 
     viz_utils.visualize_boxes_and_labels_on_image_array(
-                image_np_with_detections,
-                detections['detection_boxes'],
-                detections['detection_classes']+label_id_offset,
-                detections['detection_scores'],
-                category_index,
+                imagem_np_com_detecoes,
+                detecoes['detection_boxes'],
+                detecoes['detection_classes']+label_id_desvio,
+                detecoes['detection_scores'],
+                categoria,
                 use_normalized_coordinates=True,
                 max_boxes_to_draw=5,
                 min_score_thresh=.8,
                 agnostic_mode=False)
     
     try:
-        scores = list(filter(lambda x: x> detection_threshold, detections['detection_scores']))
-        if(scores[0] >.8):
-            text, region = ocr_it(image_np_with_detections, detections, detection_threshold, region_threshold)
-            if(times == 0):
-                times = times+1
+        valor = 0
+        pontos = list(filter(lambda x: x> detecao_limite, detecoes['detection_scores']))
+        if(pontos[0] >.8):
+            texto, regiao = ocr_it(imagem_np_com_detecoes, detecoes, detecao_limite, regiao_limite)
+            if(tempos == 0):
+                tempos = tempos+1
                 pass
             else:
                 try:
-                    texto_formatado = "".join(text)
+                    texto_formatado = "".join(texto)
                     if(texto_formatado == ""):
                         pass
                     else:
                         texto_limpo = re.sub(r'\W+', '', texto_formatado).upper()
                         data_hora = datetime.datetime.now ()
                         data_hora_formatada = data_hora.strftime ('%Y-%m-%d %H:%M:%S')
-                        valor = find_closest_value(texto_limpo)
-                        if(valor == 0):
+                        matricula = find_closest_matricula(texto_limpo)
+                        if(matricula == 0):
                             print("ERRO: Matrícula não está registada na base de dados.")
                         else:
-                            print("foi select")
-                            sql = "SELECT pagou FROM matriculas WHERE matricula = ""'" + valor + "'"" ;"
+                            #print("foi aqui")
+                            sql = "SELECT matricula FROM subscricao WHERE matricula = ""'" + matricula + "'"" ;"
                             cursor.execute (sql)
                             for row in cursor.fetchall():
-                                value = row[0]
-                            if(value == 0):
-                                print("Não pagou o estacionamento")
+                                valor = row[0]
+                            if(valor != matricula):
+                                #print("foi matricula")
+                                sql = "SELECT pagou FROM matriculas WHERE matricula = ""'" + matricula + "'"";"
+                                cursor.execute (sql)
+                                for row in cursor.fetchall():
+                                    valor = row[0]
+                                if(valor == 0):
+                                    print("Não pagou o estacionamento")
+                                else:
+                                    if(valor == 1):
+                                        #print("foi update")
+                                        sql = "UPDATE registromatriculas SET datasaida = ""'" + data_hora_formatada + "'"" WHERE matricula = ""'" + matricula + "'"" ;"
+                                        cursor.execute (sql)
+                                        conn.commit()
+                                        sql = "DELETE FROM matriculas WHERE matricula = ""'" + matricula + "'"";"
+                                        cursor.execute(sql)
+                                        conn.commit()
                             else:
-                                if(value == 1):
-                                    print("foi update")
-                                    sql = "UPDATE registromatriculas SET datasaida = ""'" + data_hora_formatada + "'"" WHERE matricula = ""'" + valor + "'"" ;"
+                                if(valor == matricula):
+                                    #print("foi subscricao")
+                                    sql = "SELECT dataentrada FROM matriculas WHERE matricula = ""'" + matricula + "'"" ;"
                                     cursor.execute (sql)
+                                    for row in cursor.fetchall():
+                                        data_entrada = row[0]
+                                    data_formatada = data_entrada.strftime ('%Y-%m-%d %H:%M:%S')
+                                    sql = "INSERT INTO registromatriculas (matricula, dataentrada) VALUES(%s, %s);"
+                                    params = (matricula, data_formatada)
+                                    cursor.execute (sql, params)
+                                    conn.commit()
+                                    #print("foi update")
+                                    sql = "UPDATE registromatriculas SET datasaida = ""'" + data_hora_formatada + "'"" WHERE matricula = ""'" + matricula + "'"" ;"
+                                    cursor.execute (sql)
+                                    conn.commit()
+                                    sql = "DELETE FROM matriculas WHERE matricula = ""'" + matricula + "'"";"
+                                    cursor.execute(sql)
                                     conn.commit()
 
                         passagem = input("Matrícula Gravada, Passagem completa? (Y): ")
@@ -245,7 +265,7 @@ while cap.isOpened():
         # print("ERROR: " + str(ex))
         pass
 
-    cv2.imshow('object detection',  cv2.resize(image_np_with_detections, (800, 600)))
+    cv2.imshow('object detection',  cv2.resize(imagem_np_com_detecoes, (800, 600)))
     
     if cv2.waitKey(10) & 0xFF == ord('q'):
         cursor.close ()
